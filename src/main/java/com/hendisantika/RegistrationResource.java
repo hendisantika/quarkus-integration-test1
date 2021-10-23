5518be6184361f897712e269cfeff3486f3d5ee5
Hendi Santika <hendisantika@gmail.com>
2021-10-24 06:43:09 +0700

    #11 - Add insertUser API in RegistrationResource

--- src/main/java/com/hendisantika/RegistrationResource.java
+++ src/main/java/com/hendisantika/RegistrationResource.java
@@ -3,11 +3,16 @@ package com.hendisantika;
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
 import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import java.net.URI;
 
 @Path("/registration")
 public class RegistrationResource {
@@ -22,5 +27,19 @@ public class RegistrationResource {
         return User.findUserByUsername(username)
                 .map(u -> Response.ok(u).build())
                 .orElseGet(() -> Response.status(Response.Status.NOT_FOUND).build());
+    }
+
+    @POST
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response insertUser(User user) {
+        user.password = passwordGenerator.generate();
+        user.persist();
+
+        URI userUri = UriBuilder.fromResource(RegistrationResource.class)
+                .path("/" + user.id).build();
+        return Response
+                .created(userUri)
+                .build();
     }
 }
