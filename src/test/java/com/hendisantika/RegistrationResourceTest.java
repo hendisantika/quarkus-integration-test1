679d916c2358c0d3fd553c0fd53195960f92cc7e
Hendi Santika <hendisantika@gmail.com>
2021-10-24 06:51:05 +0700

    #18 - Add variable in RegistrationResourceTest

--- src/test/java/com/hendisantika/RegistrationResourceTest.java
+++ src/test/java/com/hendisantika/RegistrationResourceTest.java
@@ -1,26 +1,25 @@
 package com.hendisantika;
 
 import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.InjectMock;
 import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import java.net.URL;
 
 @QuarkusTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @TestHTTPEndpoint(RegistrationResource.class)
 public class RegistrationResourceTest {
 
-    @Test
-    public void testHelloEndpoint() {
-        given()
-                .when().get("/hello")
-                .then()
-                .statusCode(200)
-                .body(is("Hello RESTEasy"));
-    }
+    @TestHTTPResource
+    @TestHTTPEndpoint(RegistrationResource.class)
+    URL url;
+
+    @InjectMock
+    PasswordGenerator passwordGenerator;
+
 
 }
