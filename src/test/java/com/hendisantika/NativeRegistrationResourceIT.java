558e9cb4cb56d1e3fb757d13ff4921e371553e6f
Hendi Santika <hendisantika@gmail.com>
2021-10-24 06:56:16 +0700

    #21 - Add shouldRegisterAUser in NativeRegistrationResourceIT

--- src/test/java/com/hendisantika/NativeRegistrationResourceIT.java
+++ src/test/java/com/hendisantika/NativeRegistrationResourceIT.java
@@ -1,9 +1,30 @@
 package com.hendisantika;
 
 import io.quarkus.test.junit.NativeImageTest;
+import org.junit.jupiter.api.Test;
+
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import static io.restassured.RestAssured.given;
 
 @NativeImageTest
 public class NativeRegistrationResourceIT extends RegistrationResourceTest {
 
-    // Execute the same tests but in native mode.
+    @Test
+    public void shouldRegisterAUser() {
+
+        final User user = new User();
+        user.username = "naruto";
+        user.email = "naruto@example.com";
+
+        given()
+                .body(user)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+                .when().post()
+                .then()
+                .statusCode(Response.Status.CREATED.getStatusCode());
+    }
 }
