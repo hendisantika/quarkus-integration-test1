aadc05943ebc05e20738a2fd666660c532d87057
Hendi Santika <hendisantika@gmail.com>
2021-10-24 06:40:39 +0700

    #5 - Add PasswordGenerator

---
+++ src/main/java/com/hendisantika/PasswordGenerator.java
@@ -0,0 +1,14 @@
+package com.hendisantika;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : quarkus-integration-test1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 24/10/21
+ * Time: 06.40
+ */
+public interface PasswordGenerator {
+    String generate();
+}
