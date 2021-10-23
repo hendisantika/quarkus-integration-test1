658f921936a8577ddcf29f133997c9b96d5679d4
Hendi Santika <hendisantika@gmail.com>
2021-10-24 06:48:16 +0700

    #15 - Add shouldGenerateANewPassword Test Case in RegeneratePasswordTest

--- src/test/java/com/hendisantika/RegeneratePasswordTest.java
+++ src/test/java/com/hendisantika/RegeneratePasswordTest.java
@@ -1,9 +1,15 @@
 package com.hendisantika;
 
+import io.quarkus.panache.mock.PanacheMock;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.mockito.InjectMock;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import javax.inject.Inject;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,5 +29,19 @@ public class RegeneratePasswordTest {
     @Inject
     RegeneratePassword regeneratePassword;
 
+    @Test
+    public void shouldGenerateANewPassword() {
+        Mockito.when(passwordGenerator.generate()).thenReturn("my_super_password").thenReturn("password");
+
+        PanacheMock.mock(User.class);
+        User user = new User("Naruto", "naruto@example.com", "my_super_password");
+        Mockito.when(User.findUserByUsername("Alex")).thenReturn(Optional.of(user));
+
+        regeneratePassword.regenerate("Naruto");
+
+        PanacheMock.verify(User.class, Mockito.times(1)).findUserByUsername("Naruto");
+        assertThat(user.password).isEqualTo("my_super_password");
+
+    }
 
 }
