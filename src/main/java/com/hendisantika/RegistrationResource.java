c0c5da3f981adb257435210b04fc40924a246c8f
Hendi Santika <hendisantika@gmail.com>
2021-10-24 06:42:44 +0700

    #10 - Add findUserByUsername API in RegistrationResource

--- src/main/java/com/hendisantika/RegistrationResource.java
+++ src/main/java/com/hendisantika/RegistrationResource.java
@@ -1,10 +1,13 @@
 package com.hendisantika;
 
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/registration")
 public class RegistrationResource {
@@ -13,8 +16,11 @@ public class RegistrationResource {
     PasswordGenerator passwordGenerator;
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    @Path("/{username}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response findUserByUsername(@PathParam("username") String username) {
+        return User.findUserByUsername(username)
+                .map(u -> Response.ok(u).build())
+                .orElseGet(() -> Response.status(Response.Status.NOT_FOUND).build());
     }
 }
